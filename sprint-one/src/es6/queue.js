07a30c714fd34620c12ce3cd8189888be8f0e7c1
gbodeen <gabebodeen@gmail.com>
2018-11-07 19:36:03 -0600

    Add Queue in ES6 style

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.front = 0;
+    this.back = 0;
+    this.storage = {};
+  }
+
+  enqueue(value) {
+    this.storage[this.back] = value;
+    this.back++;
+  }
+
+  dequeue() {
+    if (this.size() !== 0) {
+      this.front++;
+      return this.storage[this.front - 1];
+    }
+  }
+
+  size() {
+    return this.back - this.front;
   }
 
 }
