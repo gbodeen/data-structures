eb284512e1e21b943181312414c3839ac6821c21
gbodeen <gabebodeen@gmail.com>
2018-11-08 10:40:48 -0600

    Implement LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,40 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    if (list.tail) {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    } else {
+      list.tail = Node(value);
+      list.head = list.tail;
+    }
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    let oldHead = list.head;
+    list.head = list.head.next;
+    //consider deleting oldHead
+    return oldHead.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    let current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
