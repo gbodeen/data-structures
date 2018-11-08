0633c0ff38f1bc56946e54ff84f2dfd98ba91e64
gbodeen <gabebodeen@gmail.com>
2018-11-08 17:29:39 -0600

    Implement Set in prototypal style.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,23 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  return this._storage.includes(item);
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  this._storage = this._storage.filter(elem => elem !== item);
 };
 
 /*
