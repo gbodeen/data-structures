8c70b179231fc57ea836e74939a337ce64db4fcb
gbodeen <gabebodeen@gmail.com>
2018-11-07 19:14:19 -0600

    Add Queue in functional-shared style.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
-var Queue = function() {
+var Queue = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueObject = {
+    storage: {},
+    front: 0,
+    back: 0
+  };
+  Object.assign(queueObject, queueMethods);
+  return queueObject;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (value) {
+    this.storage[this.back] = value;
+    this.back++;
+    return this.size();
+  },
+  dequeue: function () {
+    if (this.size() !== 0) {
+      this.front++;
+      return this.storage[this.front - 1];
+    }
+  },
+  size: function () {
+    return this.back - this.front;
+  }
+};
+
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,26 +1,26 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var stackObject = {counter: 0};
+  var stackObject = { counter: 0 };
   Object.assign(stackObject, stackMethods);
   return stackObject;
 };
 
 var stackMethods = {
-  push: function(value) {
+  push: function (value) {
     this[this.counter] = value;
     this.counter++;
     return this.counter;
   },
 
-  pop: function() {
+  pop: function () {
     if (this.counter !== 0) {
       this.counter--;
       return this[this.counter];
     }
   },
 
-  size: function() {
+  size: function () {
     return this.counter;
   }
 };
