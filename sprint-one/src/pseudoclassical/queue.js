c3a0951cff0f04b833cf5acd6b728b046dd48297
gbodeen <gabebodeen@gmail.com>
2018-11-07 19:32:01 -0600

    Add Queue in pseudoclassical style.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
-var Queue = function() {
+var Queue = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.front = 0;
+  this.back = 0;
+  this.storage = {};
+
 };
 
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.back] = value;
+  this.back++;
+  return this.size();
+};
 
+Queue.prototype.dequeue = function () {
+  if (this.size() !== 0) {
+    this.front++;
+    return this.storage[this.front - 1];
+  }
+};
+
+Queue.prototype.size = function () {
+  return this.back - this.front;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,23 +1,23 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.counter = 0;
 };
 
-Stack.prototype.push = function(value) {
+Stack.prototype.push = function (value) {
   this[this.counter] = value;
   this.counter++;
   return this.counter;
 };
 
-Stack.prototype.pop = function() {
+Stack.prototype.pop = function () {
   if (this.counter !== 0) {
     this.counter--;
     return this[this.counter];
   }
 };
 
-Stack.prototype.size = function() {
+Stack.prototype.size = function () {
   return this.counter;
 };
 
