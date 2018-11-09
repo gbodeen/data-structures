4cb511d24ba29c41b3410b1008e1e9279456141d
gbodeen <gabebodeen@gmail.com>
2018-11-09 10:20:11 -0600

    Implement BST Tree without balancing and advanced features

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,61 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
+    var newBST = {};
+    newBST.value = value;
+    newBST.left = null;
+    newBST.right = null;
+    Object.assign(newBST, BSTMethods);
+
+    return newBST;
 };
+
+var BSTMethods = {};
+
+BSTMethods.insert = function (value) {
+    if (value < this.value) {
+        if (this.left) {
+            this.left.insert(value);
+        } else {
+            this.left = BinarySearchTree(value);
+        }
+    } else if (value > this.value) {
+        if (this.right) {
+            this.right.insert(value);
+        } else {
+            this.right = BinarySearchTree(value);
+        }
+    }
+};
+
+BSTMethods.contains = function (value) {
+
+    //base case 
+    if (this.value === value) {
+        return true;
+    }
+    //return (!!this.left && this.left.contains(value)) ||
+    //   (!!this.right && this.right.contains(value));
+
+    if (value < this.value && this.left) {
+        return this.left.contains(value);
+    }
+    if (value > this.value && this.right) {
+        return this.right.contains(value);
+    }
+    return false;
+
+};
+
+BSTMethods.depthFirstLog = function (cb) {
+    cb(this.value);
+    if (this.left) {
+        this.left.depthFirstLog(cb);
+    }
+    if (this.right) {
+        this.right.depthFirstLog(cb);
+    }
+};
+
+
 
 
 /*
