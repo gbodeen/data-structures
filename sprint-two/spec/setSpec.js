476388ba9d0c9e9e1f9a6c76dda7aa46c6e1b3dc
gbodeen <gabebodeen@gmail.com>
2018-11-10 09:37:36 -0600

    Re-implement set to use HasTables so that lookups are constant time-nifty

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -1,27 +1,48 @@
-describe('set', function() {
+describe('set', function () {
   var set;
 
-  beforeEach(function() {
+  beforeEach(function () {
     set = Set();
   });
 
-  it('should have methods named "add", "contains", and "remove"', function() {
+  it('should have methods named "add", "contains", and "remove"', function () {
     expect(set.add).to.be.a('function');
     expect(set.contains).to.be.a('function');
     expect(set.remove).to.be.a('function');
   });
 
-  it('should add values to a set', function() {
+  it('should add values to a set', function () {
     set.add('Susan Sarandon');
     set.add('Danny Glover');
     expect(set.contains('Danny Glover')).to.equal(true);
     expect(set.contains('Susan Sarandon')).to.equal(true);
   });
 
-  it('should remove values from a set', function() {
+  it('should remove values from a set', function () {
     set.add('Mel Gibson');
     set.remove('Mel Gibson');
     expect(set.contains('Mel Gibson')).to.equal(false);
+  });
+
+  it('should handle numbers as well as strings', function () {
+    set.add(-42.5);
+    set.add(0);
+    set.add(14);
+    set.remove(14);
+    expect(set.contains(-42.5)).to.equal(true);
+    expect(set.contains(0)).to.equal(true);
+    expect(set.contains(14)).to.equal(false);
+  });
+
+  it('should handle input objects of any type', function () {
+    set.add([]);
+    set.add([4, 5, 6]);
+    set.add({ a: 'apple', b: 'banana' });
+    set.add(function (x) { return 2 * x; });
+    expect(set.contains([])).to.equal(true);
+    expect(set.contains([4, 5, 6])).to.equal(true);
+    expect(set.contains({ a: 'apple', b: 'banana' })).to.equal(true);
+    expect(set.contains(function (x) { return 2 * x; })).to.equal(true);
   });
 
 });
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,25 +1,35 @@
 var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = [];
+  set._storage = new HashTable();
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function (item) {
-  if (!this.contains(item)) {
-    this._storage.push(item);
-  }
+  this._storage.insert(this.objToKey(item), item);
+  // if (!this.contains(item)) {
+  //   this._storage.push(item);
+  // }
 };
 
 setPrototype.contains = function (item) {
-  return this._storage.includes(item);
+  return this._storage.retrieve(this.objToKey(item)) !== undefined;
+  // return this._storage.includes(item);
 };
 
 setPrototype.remove = function (item) {
-  this._storage = this._storage.filter(elem => elem !== item);
+  this._storage.remove(this.objToKey(item));
+  // this._storage = this._storage.filter(elem => elem !== item);
 };
 
+setPrototype.objToKey = function (item) {
+  if (typeof item === 'function') {
+    return item.toString();
+  } else {
+    return JSON.stringify(item);
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
